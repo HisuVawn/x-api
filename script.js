278a88aba769335503f6400f4edeadf5cffe0a6e
hieu.van <luvanhieu96@gmail.com>
2024-08-07 21:24:52 +0700

    [update]

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-// Replace these with your own Twitter API credentials
-const apiKey = 'DqR5BKurvwBitPU2Of2slV2QM';
-const apiSecretKey = 'Ua8O2qzWa89PqvGuCJMHj2VpMsertN8ate2hlKuFsBKFvVOIMY';
-const accessToken = '1821119631295442945-7Ux1tV4jHNXmAvj3sBTVuvQY5L1C2s';
-const accessTokenSecret = 'A39pFimUlXSbPBqJxOXEZ86ddKvL5ubF8veR3JosV3TBO';
-
-// Replace with the ID of the tweet you want to track
 const tweetId = '1821129857994207255';
 
-// Function to fetch the retweet count
-async function getRetweetCount() {
-    const url = `https://api.twitter.com/2/tweets/${tweetId}?tweet.fields=public_metrics`;
+// Phương pháp 1: Sử dụng Twitter Widget
+window.onload = function() {
+    twttr.widgets.createTweet(
+        tweetId,
+        document.getElementById('tweet'),
+        {
+            align: 'center'
+        }
+    );
+};
 
+// Phương pháp 2: Sử dụng API call (không khuyến nghị cho production)
+const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+const apiUrl = `https://api.twitter.com/2/tweets/${tweetId}?tweet.fields=public_metrics`;
+
+async function getRetweetCount() {
     try {
-        const response = await fetch(url, {
+        const response = await fetch(corsProxy + apiUrl, {
             headers: {
-                'Authorization': `Bearer ${accessToken}`
+                'Authorization': 'Bearer YOUR_BEARER_TOKEN' // Thay thế bằng token thực của bạn
             }
         });
 
@@ -32,8 +37,4 @@ async function getRetweetCount() {
     }
 }
 
-// Call the function when the page loads
 getRetweetCount();
-
-// Update the count every 5 minutes (300000 milliseconds)
-setInterval(getRetweetCount, 300000);
